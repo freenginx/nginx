43413de556058f8cc2cc214005e1e2ac017428d4
Maxim Dounin <mdounin@mdounin.ru>
2026-06-20 06:14:30 +0300

    HTTP/2: improved error messages about too long headers.
    Error logging is limited to NGX_MAX_ERROR_STR (2048 bytes), and trying
    to fully log headers which are longer than NGX_HTTP_V2_MAX_FIELD (~2
    megabytes) doesn't make sense and will hide important details about the
    request if the error will indeed happen.  Instead, we now log only first
    256 bytes of the too long name or value.

--- src/http/v2/ngx_http_v2_filter_module.c
+++ src/http/v2/ngx_http_v2_filter_module.c
@@ -370,15 +370,15 @@ ngx_http_v2_header_filter(ngx_http_request_t *r)
 
         if (header[i].key.len > NGX_HTTP_V2_MAX_FIELD) {
             ngx_log_error(NGX_LOG_CRIT, fc->log, 0,
-                          "too long response header name: \"%V\"",
-                          &header[i].key);
+                          "too long response header name: \"%*s...\"",
+                          256, header[i].key.data);
             return NGX_ERROR;
         }
 
         if (header[i].value.len > NGX_HTTP_V2_MAX_FIELD) {
             ngx_log_error(NGX_LOG_CRIT, fc->log, 0,
-                          "too long response header value: \"%V: %V\"",
-                          &header[i].key, &header[i].value);
+                          "too long response header value: \"%V: %*s...\"",
+                          &header[i].key, 256, header[i].value.data);
             return NGX_ERROR;
         }
 
@@ -778,15 +778,15 @@ ngx_http_v2_create_trailers_frame(ngx_http_request_t *r)
 
         if (header[i].key.len > NGX_HTTP_V2_MAX_FIELD) {
             ngx_log_error(NGX_LOG_CRIT, fc->log, 0,
-                          "too long response trailer name: \"%V\"",
-                          &header[i].key);
+                          "too long response trailer name: \"%*s...\"",
+                          256, header[i].key.data);
             return NULL;
         }
 
         if (header[i].value.len > NGX_HTTP_V2_MAX_FIELD) {
             ngx_log_error(NGX_LOG_CRIT, fc->log, 0,
-                          "too long response trailer value: \"%V: %V\"",
-                          &header[i].key, &header[i].value);
+                          "too long response trailer value: \"%V: %*s...\"",
+                          &header[i].key, 256, header[i].value.data);
             return NULL;
         }
 
